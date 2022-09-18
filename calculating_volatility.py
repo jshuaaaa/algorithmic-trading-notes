2620ad4217c41d62bab7337cfd457ba0c8abec50
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-09-18 19:03:00 -0400

    calculating annualized volatility

---
+++ calculating_volatility.py
@@ -0,0 +1,36 @@
+
+import yfinance as yf
+import numpy as np
+import talib
+import copy
+
+stocks = ["AMZN", "META", "GOOG"]
+clhv = {}
+
+
+for ticker in stocks:
+    temp = yf.download(ticker, period='7mo', interval='1d')
+    temp.dropna(how="any", inplace=True)
+    clhv[ticker] = temp
+    
+def volatility(DF):
+    df = DF.copy()
+    df["return"] =  df["Adj Close"].pct_change()
+    vol = df["return"].std() * np.sqrt(252)
+    return vol
+
+for ticker in clhv:
+    print("Volatility of {} = {}".format(ticker, volatility(clhv[ticker])))
+
+# Calculating volatility of given stock data
+
+
+
+
+
+
+
+
+
+    
+    
